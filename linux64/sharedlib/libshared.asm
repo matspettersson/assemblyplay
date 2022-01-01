83968c6cd801dfc1917a2aa0c56b1959061f4a75
Mats Pettersson <mats.pettersson@gmail.com>
2022-01-01 21:01:20 +0100

    labb

---
+++ linux64/sharedlib/libshared.asm
@@ -0,0 +1,19 @@
+global hello
+
+section .rodata
+msg:      db "Hello, world from shared lib! Linux 64-bit", 13, 10
+msglen:   equ $ - msg
+
+section .text
+
+hello:
+  mov rax, 1        ;  write
+  mov rdi, 1        ;  STDOUT
+  mov rsi, msg
+  mov rdx, msglen
+  syscall
+
+;  mov rax, 60       ; exit
+  ;mov rdi, 0        ; rc 0
+  ;syscall
+ret
