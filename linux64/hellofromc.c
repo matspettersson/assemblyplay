58d1065d71c597f6590421049de1bb9a458e4979
Mats <mats@ubuntu.com>
2022-12-25 20:38:22 +0100

    221225

--- linux64/hellofromc.c
+++ linux64/hellofromc.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 
 extern int helloint(int a);
+extern int helloints(int , int , int , int , int , int , int , int , int );
 
 int main(int argc, char *argv[]) {
 char *hello = "Hello, world! Linux 64-bit";
@@ -25,4 +26,8 @@ int rc = 0;
 	a = 8;
 	rc = helloint(a);
 	printf("Return value: %d, Integer a = %d is %s\n", rc, a, rc ? "odd" : "even");
+	rc = helloints(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	printf("rc=%d\n", rc);
+	rc = helloints(1, 1, 1, 1, 1, 5, 0, 0, 0);
+	printf("rc=%d\n", rc);
 }
