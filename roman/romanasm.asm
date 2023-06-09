36bec07e690d83a29a0d3f1975f08f5d9685cf8c
Mats <mats.pettersson@gmail.com>
2023-06-09 20:24:30 +0200

    test 230609

--- roman/romanasm.asm
+++ roman/romanasm.asm
@@ -2,6 +2,14 @@ global romanToInt:function
 
 ; nasm -f elf64 -gdwarf fiboasm.asm -o fiboasm.o
 
+;%ifidn __OUTPUT_FORMAT__, win64 
+;      mov rax, rcx
+;%elifidn __OUTPUT_FORMAT__, elf64 
+;      mov rax, rdi                ; linux rdi, win rcx
+;      mov rcx, rdi
+;%endif
+
+
 section .bss
 
 
@@ -9,27 +17,28 @@ section .rodata
 roman db    'I', 'V', 'X', 'L', 'C', 'D', 'M', 0
 arab  dq    1, 5, 10, 50, 100, 500, 1000, 0
 
-
 section .text
-
-
 
 ; rdi = str
 romanToInt:
+      xor   rax, rax
+      xor   r10, r10
+      xor   r11, r11
       xor   rcx, rcx
+      mov   r8, roman
+      mov   r10, arab
 loopstr:
       xor   rbx, rbx
       xor   rdx, rdx
 
-      ;mov   bl, [rdi + rcx]
-      ;cmp   bl, 0
       cmp   byte [rdi + rcx], 0     ;Check the end of string
       jz    end
 
-      mov   r8, roman
       xor   rdx, rdx
 looproman:
-      cmp   byte [r8 + rdx], 0     ;Check the end of string
+      xor   r9, r9
+      mov   r11b, r9b               ; Save previous value
+      cmp   byte [r8 + rdx], 0      ; Check the end of array
       jle   endroman
       mov   r9b, [r8 + rdx]
       cmp   byte [rdi + rcx], r9b
@@ -37,37 +46,39 @@ looproman:
       inc   rdx
       jmp   looproman
 e2:   
-      mov   r10, arab
-      mov   rcx, [r10 + rdx *8]
-      jmp   end
+
+      cmp   r11b, r9b
+      jae   e3
+      ;neg   r9b
+      ;add   rax, r9
+      ;add   rax, r9
+e3:
+      mov   rax, [r10 + rdx * 8]
+      jmp   end2
+      push  rax
+
 endroman:
       inc   rcx                     ;increment our counter
       jmp   loopstr
 
-;e1:   mov   rcx, rdx
+;e1:   
+      ;mov   rcx, rdx
 end:
-      mov   rax, rcx
-
+      jmp   end2
+      dec   rcx
+ ;     mov   rax, rcx
+ ;     jmp   end2
+      xor   rbx, rbx
+      pop   rbx
+      cmp   rbx, rax
+      jge   end1
+      neg   rbx
+      cmp   rcx, 0
+      jae    end
+end1:
+      add   rax, rbx
+      ;mov   rax, rcx
+      jmp   end
+end2:
       ret
-
-
-fib:
-
-%ifidn __OUTPUT_FORMAT__, win64 
-      mov rax, rcx
-%elifidn __OUTPUT_FORMAT__, elf64 
-      mov rax, rdi                ; linux rdi, win rcx
-      mov rcx, rdi
-%endif
-        sub rcx, 1
-        jle fib_done            ; f(0)=0, f(1)=1
-        xor rbx, rbx            ; f(n-1)
-        mov rax, 1              ; f(n)
-fib_loop:
-        xadd rax, rbx
-        sub rcx, 1
-        jnz fib_loop
-
-fib_done:
-        ret
 
