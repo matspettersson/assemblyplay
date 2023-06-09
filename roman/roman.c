86d75bf1eab3372c8c3e83fd97da5314ac98a369
Mats <mats.pettersson@gmail.com>
2023-06-09 22:17:11 +0200

    asked chatgpt 230609 (but that code did not work)

--- roman/roman.c
+++ roman/roman.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 extern int romanToInt(char * );
+extern int roman_to_int(char * );
+extern int validate_roman_numeral(char * );
 extern int fib(int );
 
 
@@ -9,8 +11,14 @@ int main(int argc, char *argv[]) {
 
 if(argc == 2) {
 	printf("Roman number: %s\n", argv[1]);
-	int rc = romanToInt(argv[1]);
+	// int rc = romanToInt(argv[1]);
+	int rc = roman_to_int("XII");
 	printf("rc=%d\n", rc);
+
+
+	rc = validate_roman_numeral("XII");
+	printf("rc validate=%d\n", rc);
+
 } else { 
 	printf("Enter roman number as parameter...\n");
 }
