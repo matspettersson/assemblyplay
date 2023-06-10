7af77840e861f5f64f823dbbee9cfe9ec86966d4
Mats <mats.pettersson@gmail.com>
2023-06-10 20:40:07 +0200

    almost works...

--- roman/roman.c
+++ roman/roman.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 
 extern int romanToInt(char * );
-extern int roman_to_int(char * );
-extern int validate_roman_numeral(char * );
-extern int fib(int );
-
+extern int broman_to_int(char * );
+extern int bvalidate_roman_numeral(char * );
+// gdb --args roman X
+// x/s $rdi
+// p $rcx
 
 int main(int argc, char *argv[]) {
 
 if(argc == 2) {
 	printf("Roman number: %s\n", argv[1]);
-	// int rc = romanToInt(argv[1]);
-	int rc = roman_to_int("XII");
+	int rc = romanToInt(argv[1]);
 	printf("rc=%d\n", rc);
+	//rc = broman_to_int(argv[1]);
+	//printf("rc=%d\n", rc);
 
 
-	rc = validate_roman_numeral("XII");
-	printf("rc validate=%d\n", rc);
+//	rc = bvalidate_roman_numeral(argv[1]);
+//	printf("rc validate=%d\n", rc);
 
 } else { 
 	printf("Enter roman number as parameter...\n");
--- roman/romanasm.asm
+++ roman/romanasm.asm
@@ -1,5 +1,6 @@
 global romanToInt:function
-
+global broman_to_int:function
+global bvalidate_roman_numeral:function
 ; nasm -f elf64 -gdwarf fiboasm.asm -o fiboasm.o
 
 ;%ifidn __OUTPUT_FORMAT__, win64 
@@ -9,13 +10,15 @@ global romanToInt:function
 ;      mov rcx, rdi
 ;%endif
 
-
 section .bss
-
 
 section .rodata
 roman db    'I', 'V', 'X', 'L', 'C', 'D', 'M', 0
 arab  dq    1, 5, 10, 50, 100, 500, 1000, 0
+roman_numerals db "IVXLCDM"    ; Roman numeral symbols
+num_symbols equ ($ - roman_numerals)    ; Number of valid Roman numeral symbols
+values        dw 1, 5, 10, 50, 100, 500, 1000   ; Corresponding integer values
+
 
 section .text
 
@@ -48,14 +51,15 @@ looproman:
 e2:   
 
       cmp   r11b, r9b
-      jae   e3
+      jge   e3
+      ;sub   rax, r9
       ;neg   r9b
       ;add   rax, r9
       ;add   rax, r9
 e3:
-      mov   rax, [r10 + rdx * 8]
-      jmp   end2
-      push  rax
+      add   rax, [r10 + rdx * 8]
+      ;jmp   end2
+      ;push  rax
 
 endroman:
       inc   rcx                     ;increment our counter
@@ -82,3 +86,127 @@ end1:
 end2:
       ret
 
+
+broman_to_int:
+    push rbp
+    mov rbp, rsp
+
+    ; Initialize variables
+    mov rsi, rdi            ; Pointer to input string
+    xor rax, rax            ; Result
+    xor rcx, rcx            ; Previous value
+
+convert_loop:
+    movzx edx, byte [rsi]   ; Current symbol
+    cmp edx, 0              ; Check for null terminator
+    je end_conversion
+
+    ; Find value of the current symbol
+    xor rdi, rdi            ; Index of current symbol
+    movzx edi, dl
+    mov bx, word [values + rdi * 2]
+    ;mov rbx,  [values + rdi * 2]
+    ;mov rbx,  [arab + rdi * 8]
+
+    ; Compare with the previous value
+    ;cmp bx, cx
+    cmp rbx, rcx
+    jge add_value
+    sub rax, rbx
+    jmp next_iteration
+
+add_value:
+    add rax, rbx
+
+next_iteration:
+    inc rsi
+    mov rcx, rbx
+    jmp convert_loop
+
+end_conversion:
+    pop rbp
+    ret
+
+
+bvalidate_roman_numeral:
+    push rbp
+    mov rbp, rsp
+
+    ; Initialize variables
+    mov rsi, rdi    ; Pointer to input string
+    xor eax, eax    ; Result
+
+    ; Check for valid Roman numeral
+    call validate_symbols
+    jz validate_subtraction
+    jmp invalid_numeral
+
+validate_symbols:
+    movzx edx, byte [rsi]    ; Current symbol
+    cmp edx, 0    ; Check for null terminator
+    je valid_symbols
+
+    ; Check if the current symbol is a valid Roman numeral symbol
+    xor rdi, rdi    ; Index of current symbol
+    mov ecx, num_symbols
+    xor ebx, ebx    ; Found flag
+
+    search_symbol:
+    cmp dl, byte [roman_numerals + rdi]
+    je symbol_found
+
+    inc rdi
+    loop search_symbol
+
+    jmp invalid_symbols
+
+symbol_found:
+    mov ebx, 1
+    jmp validate_symbols
+
+invalid_symbols:
+    xor eax, eax    ; Invalid Roman numeral symbols
+    jmp end_validation
+
+valid_symbols:
+    xor ebx, ebx    ; Reset found flag
+    jmp validate_symbols
+
+validate_subtraction:
+    movzx edx, byte [rsi + 1]    ; Next symbol
+    cmp edx, 0    ; Check for null terminator
+    je valid_subtraction
+
+    ; Check if the current symbol can be subtracted from the next symbol
+    xor rdi, rdi    ; Index of current symbol
+    mov ecx, num_symbols
+    xor ebx, ebx    ; Found flag
+
+    search_subtraction:
+    cmp dl, byte [roman_numerals + rdi]
+    je subtraction_found
+
+    inc rdi
+    loop search_subtraction
+
+    jmp invalid_subtraction
+
+subtraction_found:
+    mov ebx, 1
+    jmp validate_subtraction
+
+invalid_subtraction:
+    xor eax, eax    ; Invalid Roman numeral subtraction
+    jmp end_validation
+
+valid_subtraction:
+    xor ebx, ebx    ; Reset found flag
+    jmp validate_subtraction
+
+invalid_numeral:
+    xor eax, eax    ; Invalid Roman numeral
+    jmp end_validation
+
+end_validation:
+    pop rbp
+    ret
