4c0c7e2c839f132ca7a89f57ce98dd69cdfbd296
Mats <mats.pettersson@gmail.com>
2023-06-11 21:46:15 +0200

    just testing

--- roman/romanasm.asm
+++ roman/romanasm.asm
@@ -20,22 +20,31 @@ values        dw 1, 5, 10, 50, 100, 500, 1000   ; Corresponding integer values
 
 section .text
 
+
+
+
+
 ; rdi = str
 romanToInt:
       xor   rax, rax
       xor   r10, r10
       xor   r11, r11
       xor   r12, r12
-      mov   r12, 9999
+      ;mov   r12, 9999
       xor   rcx, rcx
       mov   r8, roman
       mov   r9, arab
+
+      call  strlen                  ; length of rdi
+      mov   rcx, rax                
+
 loopstr:
       xor   rbx, rbx
       xor   rdx, rdx
 
-      cmp   byte [rdi + rcx], 0         ; Check the end of string
-      jz    end
+;      cmp   byte [rdi + rcx], 0         ; Check the end of string
+      cmp   rcx, 0
+      jge    end
 
 looproman:
         cmp   byte [r8 + rdx], 0            ; Check the end of array
@@ -48,16 +57,36 @@ looproman:
 romanmatch:
         mov   r11,[r9 + rdx * 8]
         cmp     r11, r12
-        jle     addval
+        jge     addval
         sub     rax, r11
         jmp     endroman
 addval:
         add rax, r11    ;[r9 + rdx * 8]             ; Add to rax
 
 endroman:
-      inc   rcx                     ; increment counter
+;      inc   rcx                     ; increment counter
+      dec   rcx
         mov     r12, r11        ; save current val 
       jmp   loopstr
 
 end:
       ret
+
+
+
+
+strlen:         ;(rdi = *)
+      push  rcx             ; save and clear out counter
+      push  rdi             ;
+      xor   rcx, rcx        ;
+strlen_next:
+      cmp   [rdi], byte 0   ; null byte yet?
+      jz    strlen_null     ; yes, get out
+      inc   rcx             ; char is ok, count it
+      inc   rdi             ; move to next char
+      jmp   strlen_next     ; process again
+strlen_null:
+      mov   rax, rcx        ; rcx = the length (put in rax)
+      pop   rdi
+      pop   rcx             ; restore rcx
+      ret   
