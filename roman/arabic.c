24f989951c2f5aca521dd18c11acf9b5e7b30375
Mats <mats.pettersson@gmail.com>
2023-07-07 17:40:47 +0200

    just testing

--- roman/arabic.c
+++ roman/arabic.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern char *  arabicToString(int);
-//extern char *  getPointerToRoman(int);
-//extern int  arabicToString(int);
+extern char *arabicToString(int);
+extern int romanToInt(char *);
 // gdb --args roman X
 // x/s $rdi
 // p $rcx
 
 int main(int argc, char *argv[]) {
 
-if(argc == 2) {
-	printf("Arabic number: %s\n", argv[1]);
-	int a = atoi(argv[1]);
-	char *str = arabicToString(a);
-	printf("str=%s\n", str);
-
-/*	char *s2 = NULL;
-	int i;
-	for(i = 0; i < 12; i++) {
-		 s2 = getPointerToRoman(i);
-		printf("str=%s\n", s2);
+if(argc == 3) {
+	if(strcmp(argv[1], "A") == 0) {
+		printf("Arabic number: %s\n", argv[2]);
+		int a = atoi(argv[2]);
+		char *str = arabicToString(a);
+		printf("str=%s\n", str);
 	}
-*/
-//	int b = arabicToString(a);
-//	printf("ret=%d\n", b);
 
-	//rc = broman_to_int(argv[1]);
-	//printf("rc=%d\n", rc);
-
-
-//	rc = bvalidate_roman_numeral(argv[1]);
-//	printf("rc validate=%d\n", rc);
-
+	if(strcmp(argv[1], "R") == 0) {
+		printf("Roman number: %s\n", argv[2]);
+		int n  = romanToInt(argv[2]);
+		printf("int=%d\n", n);
+	}
 } else { 
-	printf("Enter arabic number as parameter...\n");
+	printf("Enter A or R + number...\n");
 }
+/*
+char *s0 = "CMIV";
+char *s1 = "IV";
+char *s2 = "MDMCCIV";
+char *s3 = "VIII";
+printf("%s = %d\n", s0, romanToInt(s0));
+printf("%s = %d\n", s1, romanToInt(s1));
 
+printf("%s = %d\n", s2, romanToInt(s2));
+printf("%s = %d\n", s3, romanToInt(s3));
+*/
 }
 
--- roman/arabicasm.asm
+++ roman/arabicasm.asm
@@ -1,4 +1,5 @@
 global arabicToString:function
+global romanToInt:function
 
 ; No more than 3 of the same
 ; Max: 3999 = MMMCMXCIX
@@ -12,8 +13,13 @@ section .rodata
 roman       db    'M', 0, 'CM', 0, 'D', 0, 'CD', 0, 'C', 0, 'XC', 0, 'L', 0, 'XL', 0, 'X', 0, 'IX', 0, 'V', 0, 'IV', 0, 'I', 0      ; Roman symbols
 arabic      dw    1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1, 0           ; Corresponding integer values
 
+r_roman     db    'CM', 0, 'CD', 0, 'XC', 0, 'XL', 0, 'IX', 0, 'IV', 0, 'M', 0, 'D', 0, 'C', 0, 'L', 0, 'X', 0, 'V', 0, 'I', 0      ; Roman symbols
+r_romanlen  equ   $-r_roman
+r_arabic    dw    900, 400, 90, 40, 9, 4, 1000, 500, 100, 50, 10, 5, 1, 0           ; Corresponding integer values
+
 
 section .text
+
 
 ; rdi = int
 arabicToString:
@@ -98,3 +104,139 @@ copyR1:
 copyRomanDone:
       mov   rax, rcx
 	ret
+
+
+
+
+; rdi = str
+romanToInt:
+      xor   rax, rax
+      xor   r10, r10
+      xor   r11, r11
+      xor   r12, r12
+      xor   r13, r13
+      xor   r14, r14
+      xor   r15, r15
+      xor   rcx, rcx
+      mov   r8, r_roman
+      mov   r9, r_arabic
+
+      call  strlen                                    ; length of string pased to function, in rdi
+      mov   rcx, rax                                  ; save in rcx
+
+      xor   rax, rax
+      xor   rbx, rbx
+      xor   rdx, rdx
+
+      cmp   rcx, 0
+      je    endr
+
+looproman:
+      cmp   rbx, rcx                            ; # of characters
+      jge    endr
+
+;      cmp   r15, 8                              ; 8 entries in the "array"
+;      jge   end_of_array
+
+      mov   r10, r8                             ; r10 = points to base of roman array
+      add   r10, r11                          ; r10 plus a pointer to next string in roman array
+      push  rdi
+      mov   rdi, r10
+      call  strlen
+      mov   r14, rax                            ; r14 = strlen
+      pop   rdi
+
+      push  rdi
+      push  rdx
+      add   rdi, rbx                           ; Iterate through the string argument
+      mov   rsi, r8
+      add   rsi, r11
+      mov   rdx, r14
+      call  strncmp
+
+      pop   rdx
+      pop   rdi
+
+      cmp   rax, 0
+      je    romanmatch
+
+      inc   r15
+      inc   r14               ; add null
+      add   r11, r14      
+      add   rdx, r14
+
+      jmp   looproman
+romanmatch:
+      xor   r12, r12
+      mov   r12w, word[r9 + r15 * 2]
+      add   r13, r12                                  ; Add sum to r13
+
+      add   rbx, r14
+      xor   rdx, rdx                      ; reset array index
+      xor   r11, r11
+      xor   r15, r15
+
+      jmp   looproman
+endr:
+      mov   rax, r13
+      ret                                             ; return value in rax
+end_of_array:
+      xor   r15, r15
+      jmp   looproman
+
+
+; strlen implementation using basic x86 instructions
+; Inputs:
+;   rdi - pointer to the string
+strlen:         ;(rdi = *)
+      push  rcx                                       ; save and clear out counter
+      push  rdi
+      xor   rcx, rcx
+strlen_next:
+      cmp   [rdi + rcx], byte 0                             ; null byte yet?
+      jz    strlen_null                               ; yes, get out
+      inc   rcx                                       ; char is ok, count it
+;      inc   rdi                                       ; move to next char
+      jmp   strlen_next                               ; process again
+strlen_null:
+      mov   rax, rcx                                  ; rcx = the length (put in rax)
+      pop   rdi
+      pop   rcx                                       ; restore rcx
+      ret   
+
+
+
+; strncmp implementation using basic x86 instructions
+; Inputs:
+;   rdi - pointer to the first string
+;   rsi - pointer to the second string
+;   rdx - maximum number of characters to compare
+
+strncmp:
+      push  rbx
+      push  rcx
+      xor rax, rax        ; result
+      xor rcx, rcx        ; counter
+
+strncmp_loop:
+    cmp rcx, rdx        ; check if maximum number of characters to compare reached
+    jge end_strncmp
+
+
+    mov bl, byte [rdi + rcx]    ; load character from the first string
+    mov bh, byte [rsi + rcx]    ; load character from the second string
+    cmp bl, bh                  ; compare characters
+    jne mismatch                ; jump if characters don't match
+
+    inc rcx                     ; increment counter
+    jmp strncmp_loop
+
+mismatch:
+;    sub al, dl                  ; calculate the difference
+    mov rax, 1                  ; set result to a non-zero value indicating mismatch
+;    jmp end_comparison_b
+
+end_strncmp:
+    pop     rcx
+    pop     rbx
+    ret
